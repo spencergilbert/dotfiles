bca445473207118e4b9f65e769d7a70d9eb38601
Spencer Gilbert <spencer.gilbert@gmail.com>
2019-07-22 21:01:30 -0400

    Add basic script for apt packages

---
+++ scripts/apt.sh
@@ -0,0 +1,18 @@
+#bin/sh
+
+CODENAME=`lsb_release --codename | cut -f2`
+
+apt-get update
+apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+add-apt-repository ppa:neovim-ppa/stable
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu ${CODENAME} stable"
+
+apt-get update
+apt-get install -y stow zsh tmux htop alacritty neovim docker-ce
+
+usermod -aG docker ${USER}
+
+su - ${USER}
