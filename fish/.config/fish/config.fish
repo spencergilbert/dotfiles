dafd411dc7cf1887232e76e3406cf8ce268283b3
Spencer Gilbert <spencer.gilbert@gmail.com>
2020-10-03 21:18:56 -0400

    new font, new starship conf

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,1 +1,5 @@
 source ~/.asdf/asdf.fish
+source ~/.cargo/env
+
+zoxide init fish | source
+starship init fish | source
--- install.sh
+++ install.sh
@@ -17,8 +17,12 @@ ASDF_TAG="v0.8.0"
 #
 
 aptpkgs=(
+	"alacritty"
+	"build-essential"
+	"curl"
 	"fish"
 	"git"
+	"libssl-dev"
 	"stow"
 )
 sudo apt-get update -qq
@@ -31,9 +35,9 @@ sudo apt-get install -qq -y "${aptpkgs[@]}"
 # |_|   |_|\__,_|\__| .__/ \__,_|_|\_\
 #                   |_|       
 
-flatpak install -y flathub com.discordapp.Discord
-flatpak install -y flathub com.github.johnfactotum.Foliate
-flatpak install -y flathub com.slack.Slack
+flatpak install -y --or-update flathub com.discordapp.Discord
+flatpak install -y --or-update flathub com.github.johnfactotum.Foliate
+flatpak install -y --or-update flathub com.slack.Slack
 
 #                _  __ 
 #   __ _ ___  __| |/ _|
@@ -48,6 +52,34 @@ fi
 ASDF_BIN=$(which asdf)
 
 "$ASDF_BIN" plugin-add golang https://github.com/kennyp/asdf-golang.git
+
+#   ____           _            
+#  / ___|_ __ __ _| |_ ___  ___ 
+# | |   | '__/ _` | __/ _ \/ __|
+# | |___| | | (_| | ||  __/\__ \
+#  \____|_|  \__,_|\__\___||___/
+#                               
+
+crates=(
+	"bandwhich"
+	"bat"
+	"bottom"
+	"exa"
+	"fd-find"
+	"git-delta"
+	"hyperfine"
+	"procs"
+	"ripgrep"
+	"sd"
+	"starship"
+	"tealdeer"
+	"tokei"
+	"watchexec"
+	"zoxide"
+)
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+source ~/.cargo/env
+for i in "${crates[@]}"; do cargo install "$i"; done
 
 #  ____  _                
 # / ___|| |_ _____      __
@@ -65,6 +97,7 @@ stow fish
 stow git
 stow gnupg
 stow ssh
+#stow starship
 
 #                   __ _                      
 #   ___ ___  _ __  / _(_) __ _ _   _ _ __ ___ 
@@ -72,6 +105,8 @@ stow ssh
 # | (_| (_) | | | |  _| | (_| | |_| | | |  __/
 #  \___\___/|_| |_|_| |_|\__, |\__,_|_|  \___|
 #                        |___/                
+
+sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep $(which bandwhich)
 
 sudo chsh -s $(which fish)
 
