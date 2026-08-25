23379ae33291aff276a8959700bb7f39636f1c95
Spencer Gilbert <me@spencergilbert.dev>
2026-08-24 22:57:07 -0400

    [nvim] Add augroups, desc, listchars, and more filetypes for indentation

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,25 +5,30 @@ vim.cmd.colorscheme('catppuccin-frappe')
 vim.o.number = true
 vim.o.relativenumber = true
 
--- Sync clipboard between OS and Neovim. Schedule the setting after `UIEnter` because it can
--- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
+-- Sync clipboard between OS and Neovim. Schedule after `UIEnter` to avoid
+-- increasing startup-time. Remove if you want your OS clipboard independent.
 -- See `:h 'clipboard'`
+local clipboard_group = vim.api.nvim_create_augroup('clipboard', { clear = true })
 vim.api.nvim_create_autocmd('UIEnter', {
+	group = clipboard_group,
+	desc = 'Sync OS clipboard via unnamedplus',
 	callback = function()
 		vim.o.clipboard = 'unnamedplus'
 	end,
 })
 
--- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the term
 vim.o.ignorecase = true
 vim.o.smartcase = true
 
 vim.o.cursorline = true
 vim.o.scrolloff = 10
 vim.o.list = true
+vim.o.listchars = 'tab:>· ,trail:·,extends:>,precedes:<,nbsp:+,eol:$'
 
--- If performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
--- instead raise a dialog asking if you wish to save the current file(s). See `:h 'confirm'`
+-- If performing an operation that would fail due to unsaved changes (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s).
+-- See `:h 'confirm'`
 vim.o.confirm = true
 
 -- Use <Esc> to exit terminal mode
@@ -39,11 +44,23 @@ vim.keymap.set({ 'n' }, '<A-j>', '<C-w>j')
 vim.keymap.set({ 'n' }, '<A-k>', '<C-w>k')
 vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 
--- JSON: 2-space indentation to match jq's output format
-vim.api.nvim_create_augroup('json_indent', { clear = true })
+-- Highlight when yanking (copying) text. Try `yap` in normal mode to test.
+-- See `:h vim.hl.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('highlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+	group = highlight_group,
+	desc = 'Highlight yanked text briefly',
+	callback = function()
+		vim.hl.on_yank()
+	end,
+})
+
+-- Filetype-specific indentation settings
+local indent_group = vim.api.nvim_create_augroup('indent', { clear = true })
 vim.api.nvim_create_autocmd('FileType', {
-	group = 'json_indent',
-	pattern = 'json',
+	group = indent_group,
+	desc = 'Set buffer-local indentation for specific filetypes',
+	pattern = { 'json', 'yaml', 'toml' },
 	callback = function()
 		vim.bo.tabstop = 2
 		vim.bo.shiftwidth = 2
@@ -51,4 +68,3 @@ vim.api.nvim_create_autocmd('FileType', {
 		vim.bo.expandtab = true
 	end,
 })
-
