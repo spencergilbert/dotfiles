49a76a0a94f640638d2af88739713f57659ffedf
Spencer Gilbert <me@spencergilbert.dev>
2026-08-25 21:38:44 -0400

    [fish] Export the Kagi key reference, rather than the value itself

--- fish/conf.d/10-env.fish
+++ fish/conf.d/10-env.fish
@@ -11,6 +11,4 @@ set -gx CODEX_HOME $XDG_CONFIG_HOME/codex
 set -gx PI_CODING_AGENT_DIR $XDG_CONFIG_HOME/pi
 set -gx PI_CODING_AGENT_SESSION_DIR $XDG_STATE_HOME/pi/sessions
 
-if not set -q SSH_CONNECTION
-	set -gx KAGI_API_KEY "$(op read op://Private/Pi/kagi)"
-end
+set -gx KAGI_API_KEY_REF "op://Private/Pi/kagi"
